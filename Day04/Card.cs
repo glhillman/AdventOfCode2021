847d5cce59d761df2c70a4f556c8cf6cb712db7b
glhillman <glhillman@gmail.com>
2021-12-04 07:32:12 -0700

    Day04 Fun!

---
+++ Day04/Card.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+    internal class Card
+    {
+        List<int> _values;
+        List<int> _backupValues;
+        public Card(string rowValues)
+        {
+            IsActiveCard = true;
+
+            _values = new List<int>();
+            
+            AddRowValues(rowValues);
+        }
+
+        public void AddRowValues(string rowValues)
+        {
+            string [] values = rowValues.Trim().Replace("  ", " ").Split(' ');
+            for (int i = 0; i < values.Length; i++)
+            {
+                _values.Add(int.Parse(values[i]));
+            }
+
+            if (_values.Count == 25)
+            {
+                _backupValues = new List<int>(_values);
+            }
+        }
+
+        public void ResetValues()
+        {
+            _values = new List<int>(_backupValues);
+        }
+
+        public bool IsBingo(int value)
+        {
+            bool isBingo = false;
+
+            int offset = _values.IndexOf(value);
+            if (offset >= 0)
+            {
+                _values[offset] = -1; // mark the position - don't need to preserve the value that was there
+                if (IsRowAtOffsetBingo(offset) || IsColAtOffsetBingo(offset))
+                {
+                    isBingo = true;
+                    IsActiveCard = false;
+                }
+            }
+
+            return isBingo;
+        }
+
+        public bool IsActiveCard { get; set; }
+
+        public int SumUnmarked()
+        {
+            int sum = 0;
+            foreach (int value in _values)
+            {
+                if (value > 0)
+                {
+                    sum += value;
+                }
+            }
+
+            return sum;
+        }
+
+        private bool IsRowAtOffsetBingo(int offset)
+        {
+            bool isBingo = true;
+            int row = offset / 5;
+            int startRow = row * 5;
+            for (int i = startRow; i < startRow + 5; i++)
+            {
+                if (_values[i] != -1)
+                {
+                    isBingo = false;
+                    break;
+                }
+            }
+            return isBingo;
+        }
+
+        private bool IsColAtOffsetBingo(int offset)
+        {
+            bool isBingo = true;
+            int col = offset % 5;
+            for (int row = 0; row < 5; row++)
+            {
+                if (_values[row * 5 + col] != -1)
+                {
+                    isBingo = false;
+                    break;
+                }
+            }
+            return isBingo;
+        }
+
+        public void DumpCard()
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Console.Write(_values[row * 5 + col] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day04/DayClass.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+    internal class DayClass
+    {
+        List<int> _input = new List<int>();
+        List<Card> _cards = new List<Card>();
+
+        public DayClass()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            long rslt = 0;
+
+            foreach (int value in _input)
+            {
+                foreach (Card card in _cards)
+                {
+                    if (card.IsBingo(value))
+                    {
+                        int unmarkedSum = card.SumUnmarked();
+                        rslt = unmarkedSum * value;
+                        break;
+                    }
+                }
+                if (rslt != 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Part1: {0}", rslt);
+        }
+
+        public void Part2()
+        {
+
+            long rslt = 0;
+            int unmarkedSum = 0;
+            int winningValue = 0;
+
+            foreach (Card card in _cards)
+            {
+                card.ResetValues();
+            }
+
+            foreach (int value in _input)
+            {
+                foreach (Card card in _cards)
+                {
+                    if (card.IsActiveCard && card.IsBingo(value))
+                    {
+                        unmarkedSum = card.SumUnmarked();
+                        winningValue = value;
+                    }
+                }
+            }
+            rslt = unmarkedSum * winningValue;
+            Console.WriteLine("Part1: {0}", rslt);
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string? line;
+                bool processing = true;
+                StreamReader file = new StreamReader(inputFile);
+                line = file.ReadLine();
+                string[] values = line.Split(',');
+                foreach (string value in values)
+                {
+                    _input.Add(int.Parse(value));
+                }
+                file.ReadLine(); // throw away next blank line
+                line = file.ReadLine();
+                Card card = new Card(line);
+                while (processing)
+                { 
+                    line = file.ReadLine();
+                    if (line == null)
+                    {
+                        _cards.Add(card);
+                        processing = false;
+                    }
+                    else if (line.Length == 0)
+                    {
+                        _cards.Add(card);
+                        line = file.ReadLine();
+                        card = new Card(line);
+                    }
+                    else 
+                    {
+                        card.AddRowValues(line);
+                    }
+                }
+
+                file.Close();
+            }
+        }
+
+    }
+}
